d60ff149bec140091c42e4448d174f541840b0e9
zhikezhixin <1625220574@qq.com>
2024-03-27 22:05:12 +0800

    天天向上的力量

---
+++ 2024_3_27.py
@@ -0,0 +1,13 @@
+#天天向上的力量
+def dayup(df) :
+    dayup = 1.0
+    for i in range(365) :
+        if i % 7 in [6,0] :
+            dayup*=(1 - 0.01)
+        else :
+            dayup*=(1 + df)
+    return dayup
+dayfactor  = 0.01
+while dayup(dayfactor)<37.78 :
+    dayfactor +=0.001
+print("工作日努力的参数是：{:.3f}".format(dayfactor))
